d6d36b75907ed121e8c30b35628866309d89c98d
Nandan Kumar Mishra <nand16973@gmail.com>
2026-01-30 15:42:01 +0530

    final change1

---
+++ day 4/class.py
@@ -0,0 +1,32 @@
+# class Student :
+#     s_name="nitish" #6byte
+#     s_age=22 #4bytes
+# f_student=Student() #object
+# s_student=Student() #object
+# t_student=Student() #object
+
+# print(f_student.s_name)
+# print(f_student.s_age)
+
+# print(s_student.s_name)
+# print(s_student.s_age)
+# del s_student
+# print(t_student.s_name)
+# print(t_student.s_age)
+
+
+# class Student :
+#    def  __init__(self,s_name,s_age):
+#     self.s_name=s_name
+#     self.s_age=s_age
+
+# f_student=Student("nitish",21)
+# s_student=Student("suresh",22)
+# t_student=Student("naresh",22)
+# print(s_student.s_name)
+# print(f_student.s_age)
+
+
+
+
+
