71e70abc9822cdb8de25aa3c7be15cac777861f7
Nandan Kumar Mishra <nand16973@gmail.com>
2026-03-30 10:27:32 +0530

    ho gya

--- day 9/bubble.py
+++
@@ -1,9 +0,0 @@
-arr=[5,3,8,0]
-n=len(arr)
-for i in range(n-1):
-    min_index=i
-    for j in range(i,n):
-        if(arr[min_index] > arr[j]):
-            min_index=j
-    arr[i],arr[min_index]=arr[min_index],arr[i]
-print(arr)
